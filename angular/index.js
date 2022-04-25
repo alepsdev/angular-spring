9ac38f22062531c8cd6cabcf4cac73a6eb85a864
Alessandro <alessandropazdasilva@gmail.com>
2022-04-25 17:17:25 -0300

    add datas

--- angular/index.js
+++ angular/index.js
@@ -2,13 +2,13 @@ angular.module("listaTelefonica", []);
 angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope) {
     $scope.app = "Lista Telefonica";
     $scope.selecionado = "selecionado";
-
+    
     $scope.contatos = [
-        {id:1, nome: "alessandro", telefone: "99999999", cor: "yellow"},
-        {id:2, nome: "joao", telefone: "99999999", cor: "pink"},
-        {id:3, nome: "mateus", telefone: "99999999", cor: "green"},
-        {id:4, nome: "mario", telefone: "99999999", cor: "black"},
-        {id:5, nome: "gabriel", telefone: "99999999", cor: "white"}
+        {id:1, nome: "alessandro", dataNascimento: new Date('1996-04-15'), telefone: "99999999", cor: "yellow"},
+        {id:2, nome: "joao", dataNascimento: new Date('1994-05-05'), telefone: "99999999", cor: "pink"},
+        {id:3, nome: "mateus", dataNascimento: new Date('1998-07-01'), telefone: "99999999", cor: "green"},
+        {id:4, nome: "mario", dataNascimento: new Date('1991-12-24'), telefone: "99999999", cor: "black"},
+        {id:5, nome: "gabriel", dataNascimento: new Date('2002-01-01'), telefone: "99999999", cor: "white"}
     ];
 
     $scope.operadoras = [ 
@@ -36,6 +36,12 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($s
         })
     };
 
+    $scope.aniversario = function(contatos) {
+        dataAgora = new Date();
+        contato.dataNascimento
+        
+        return 
+    }
 
     
 })
